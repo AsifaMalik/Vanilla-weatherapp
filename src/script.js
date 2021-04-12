51c0ab48ebc1f73700c5afaf862f0711f8567b4d
AsifaMalik <79640921+AsifaMalik@users.noreply.github.com>
2021-04-12 16:29:04 +0200

    changed search: Murcia to Oslo

--- src/script.js
+++ src/script.js
@@ -164,6 +164,6 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink= document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-search("Murcia");
+search("Oslo");
 
 
